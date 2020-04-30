bb6c734dd204d2476f76d7195017d00419b85406
Anmol Sethi <hi@nhooyr.io>
2020-04-30 07:52:54 -0400

    Add NPM package, debs, rpms and refactor CI/build process
    Closes many issues that I'll prune after adding more docs
    for users.

--- ci/container/exec.sh
+++ ci/container/exec.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 
 main() {
@@ -16,9 +15,19 @@ main() {
   done &
   pid=$!
 
-  docker build ci/image
-  imageTag="$(docker build -q ci/image)"
-  docker run -t --rm -e CI -e GITHUB_TOKEN -e TRAVIS_TAG -v "$(yarn cache dir):/usr/local/share/.cache/yarn/v6" -v "$PWD:/repo" -w /repo "$imageTag" "$*"
+  docker build ci/container
+  imageTag="$(docker build -q ci/container)"
+  docker run \
+    --rm \
+    -e CI \
+    -e GITHUB_TOKEN \
+    -e TRAVIS_TAG \
+    -v "$(yarn cache dir):/usr/local/share/.cache/yarn/v6" \
+    -v "$PWD:/repo" \
+    -w /repo \
+    $(if [[ -t 0 ]]; then echo -it; fi) \
+    "$imageTag" \
+    "$*"
 
   kill $pid
 }
---
+++ ci/steps/static-release.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+main() {
+  cd "$(dirname "$0")/../.."
+
+  yarn
+  yarn vscode
+  yarn build
+  STATIC=1 yarn release
+  ./ci/build/test-static-release.sh
+  ./ci/build/archive-static-release.sh
+}
+
+main "$@"
